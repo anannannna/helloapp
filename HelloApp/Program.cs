5cb0d518469485255f496b0e5be8df2ec80d9587
black <black@example.com>
2025-10-31 00:44:56 +0500

    fixes

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -11,6 +11,7 @@ namespace HelloApp
         static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+            GreetBlack();
         }
 
         static void GreetWhite(){
